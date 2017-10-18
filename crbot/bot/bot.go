4f8044fa4715d3551c8c93cc7d08ba0fc6c51067
alberto__segura <alberto.segura.delgado@gmail.com>
2017-10-18 19:47:46 +0200

    crbot working

---
+++ crbot/bot/bot.go
@@ -0,0 +1,104 @@
+package bot
+
+
+import (
+    "log"
+    "encoding/hex"
+    "errors"
+    
+    "github.com/tucnak/telebot"
+
+    "github.com/segura2010/cr-go/client"
+    "github.com/segura2010/cr-go/packets"
+    "github.com/segura2010/cr-go/utils"
+
+    //"CRBot/db"
+    "CRBot/config"
+)
+
+type TelegramBot struct{
+	Bot *telebot.Bot
+    Token string
+    Started bool
+}
+
+func GetStatsForPlayer(tag string) (packets.Packet, error){
+    var err error
+    serverKey, err := hex.DecodeString(config.GetInstance().CRBot.ServerKey)
+    if err != nil{
+        log.Printf("Invalid server key")
+    }
+
+    helloPayload := packets.NewDefaultClientHello()
+    helloPkt := packets.Packet{
+        Type: packets.MessageType["ClientHello"],
+        Version: 0,
+        Payload: helloPayload.Bytes(),
+    }
+
+    loginPayload := packets.NewDefaultClientLogin()
+    HiLo := utils.Tag2HiLo(config.GetInstance().CRBot.PlayerTag) // test account
+    loginPayload.Hi = HiLo[0]
+    loginPayload.Lo = HiLo[1]
+    loginPayload.PassToken = config.GetInstance().CRBot.PassToken
+
+    loginPkt := packets.Packet{
+        Type: packets.MessageType["ClientLogin"],
+        Version: 0,
+        DecryptedPayload: loginPayload.Bytes(),
+    }
+
+    var basicPkt packets.Packet
+
+    serverAddress := "game.clashroyaleapp.com:9339"
+
+    c := client.NewCRClient(serverKey)
+    c.Connect(serverAddress)
+    defer c.Close()
+
+    c.SendPacket(helloPkt)
+    c.RecvPacket() // receive hello response
+
+    c.SendPacket(loginPkt)
+    basicPkt = c.RecvPacket() // receive login response
+    if basicPkt.Type == packets.MessageType["ServerLoginFailed"]{
+        panic("ServerLoginFailed")
+    }
+    //loginOk := packets.NewServerLoginOkFromBytes(basicPkt.DecryptedPayload)
+    log.Printf("LoginOk")
+
+    // receive multiple packets the server sends after login
+    c.RecvPacket() // OwnHomeData
+    c.RecvPacket() // InboxGlobal
+    c.RecvPacket() // FriendList
+
+    // send keepalive
+    basicPkt = packets.Packet{
+        Type: packets.MessageType["ClientKeepAlive"],
+        Version: 0,
+    }
+    c.SendPacket(basicPkt)
+    c.RecvPacket() // receive keepalive response
+
+    // send visithome
+    HiLo = utils.Tag2HiLo(tag)
+    visitHomeMsg := packets.ClientVisitHome{
+        Hi: HiLo[0],
+        Lo: HiLo[1],
+    }
+    basicPkt = packets.Packet{
+        Type: packets.MessageType["ClientVisitHome"],
+        Version: 0,
+        DecryptedPayload: visitHomeMsg.Bytes(),
+    }
+    c.SendPacket(basicPkt)
+    basicPkt = c.RecvPacket() // receive visithome response
+    if basicPkt.Type != packets.MessageType["ServerVisitedHome"]{
+        log.Printf("ServerVisitedHome not received for %s", tag)
+        return basicPkt, errors.New("ServerVisitedHome not received")
+    }
+
+    visitHomeResponse := packets.NewServerVisitHomeFromBytes(basicPkt.DecryptedPayload)
+    log.Printf("Received VisitHomeData for player %s", utils.HiLo2Tag(visitHomeResponse.Hi, visitHomeResponse.Lo))
+    return basicPkt, nil
+}
--- crbot/main.go
+++ crbot/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
     "fmt"
-    "encoding/hex"
     "flag"
+    "time"
+    "log"
+    "strings"
 
-    "github.com/segura2010/cr-go/client"
-    "github.com/segura2010/cr-go/packets"
-    "github.com/segura2010/cr-go/utils"
+    "CRBot/db"
+    "CRBot/config"
+    "CRBot/crbot/bot"
 )
 
 /* Update version number on each release:
@@ -31,82 +33,28 @@ func main(){
     cfgFile := flag.String("c", "./config.json", "Config file")
     version := flag.Bool("v", false, "Show current CRBot version")
 
+    flag.Parse()
+
     if *version{
         showVersionInfo()
     }
 
-    serverKey_202 := "980cf7bb7262b386fea61034aba7370613627919666b34e6ecf66307a381dd61"
-    serverKey,_ := hex.DecodeString(serverKey_202)
+    config.CreateInstance(*cfgFile)
 
-    helloPayload := packets.NewDefaultClientHello()
-    helloPkt := packets.Packet{
-        Type: packets.MessageType["ClientHello"],
-        Version: 0,
-        Payload: helloPayload.Bytes(),
+    for{
+        job := db.RemovePlayerStatsJob()
+        jobInfo := strings.Split(job, ":")
+        log.Printf("playerTag: %s", jobInfo[0])
+        if jobInfo[0] != ""{
+            visitHomePkt, err := bot.GetStatsForPlayer(jobInfo[0])
+            if err == nil{
+                // log.Printf("Payload: %x", visitHomePkt.DecryptedPayload)
+                msg := fmt.Sprintf("%s:%x", job, visitHomePkt.DecryptedPayload)
+                db.PublishToPlayerStats(msg)
+            }
+        }
+        time.Sleep(time.Duration(1) * time.Second)
     }
-
-    loginPayload := packets.NewDefaultClientLogin()
-    HiLo := utils.Tag2HiLo("") // test account
-    loginPayload.Hi = HiLo[0]
-    loginPayload.Lo = HiLo[1]
-    loginPayload.PassToken = ""
-
-    loginPkt := packets.Packet{
-        Type: packets.MessageType["ClientLogin"],
-        Version: 0,
-        DecryptedPayload: loginPayload.Bytes(),
-    }
-
-    var basicPkt packets.Packet
-
-    serverAddress := "0.0.0.0:9339"
-    //serverAddress := "game.clashroyaleapp.com:9339"
-
-    c := client.NewCRClient(serverKey)
-    c.Connect(serverAddress)
-    defer c.Close()
-
-    c.SendPacket(helloPkt)
-    c.RecvPacket() // receive hello response
-
-    c.SendPacket(loginPkt)
-    basicPkt = c.RecvPacket() // receive login response
-    loginOk := packets.NewServerLoginOkFromBytes(basicPkt.DecryptedPayload)
-    fmt.Printf("\nLoginOk: %v", loginOk)
-
-    // receive multiple packets the server sends after login
-    c.RecvPacket() // OwnHomeData
-    c.RecvPacket() // InboxGlobal
-    c.RecvPacket() // FriendList
-
-    // send keepalive
-    basicPkt = packets.Packet{
-        Type: packets.MessageType["ClientKeepAlive"],
-        Version: 0,
-    }
-    c.SendPacket(basicPkt)
-    c.RecvPacket() // receive keepalive response
-
-    // send visithome
-    HiLo = utils.Tag2HiLo("UY800JJ")
-    visitHomeMsg := packets.ClientVisitHome{
-        Hi: HiLo[0],
-        Lo: HiLo[1],
-    }
-    basicPkt = packets.Packet{
-        Type: packets.MessageType["ClientVisitHome"],
-        Version: 0,
-        DecryptedPayload: visitHomeMsg.Bytes(),
-    }
-    c.SendPacket(basicPkt)
-    basicPkt = c.RecvPacket() // receive visithome response
-    //fmt.Printf("\nVisitedHomeRequest: %x", visitHomeMsg.Bytes())
-    //fmt.Printf("\nVisitedHome: %x", basicPkt.DecryptedPayload)
-
-    visitHomeResponse := packets.NewServerVisitHomeFromBytes(basicPkt.DecryptedPayload)
-    fmt.Printf("\nVisitHomeMsg: %x", visitHomeMsg.Bytes())
-    fmt.Printf("\n%v", visitHomeResponse)
-    fmt.Printf("\n%s", utils.HiLo2Tag(visitHomeResponse.Hi, visitHomeResponse.Lo))
 
 }
 
